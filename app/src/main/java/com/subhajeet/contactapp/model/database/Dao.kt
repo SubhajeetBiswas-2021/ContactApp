31c2bee64b4fcd47ae17d6af17613cd2af642cee
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-02 11:50:08 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/contactapp/model/database/Dao.kt
@@ -0,0 +1,21 @@
+package com.subhajeet.contactapp.model.database
+
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface Dao {
+    @Upsert
+    suspend fun upsetContact(contact: Contact)                    //for set and update
+
+    @Delete
+    suspend fun deleteContact(contact: Contact)
+
+    @Query("SELECT * FROM contact_table ORDER BY name ASC")         //for Read purpose
+    suspend fun getAllContacts():Flow<List<Contact>>   //give it in list of contacts
+}
