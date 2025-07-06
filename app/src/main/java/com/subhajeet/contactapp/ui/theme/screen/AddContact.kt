bc910bf7311f8989e5ac17ad7e9aee85f79269eb
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-06 06:53:46 +0530

    Commit

--- app/src/main/java/com/subhajeet/contactapp/ui/theme/screen/AddContact.kt
+++ app/src/main/java/com/subhajeet/contactapp/ui/theme/screen/AddContact.kt
@@ -1,8 +1,19 @@
 package com.subhajeet.contactapp.ui.theme.screen
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -11,9 +22,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+
 import com.subhajeet.contactapp.viewModel.MyViewModel
 
 
@@ -23,10 +38,54 @@ fun AddContact(viewModel: MyViewModel?= hiltViewModel(), navController: NavContr
     val name = remember{ mutableStateOf("") }
     val phoneNumber = remember{ mutableStateOf("") }
     val email = remember{ mutableStateOf("") }
+    val image = remember { mutableStateOf<ByteArray?>(null) }//(1)
 
+    val imageUri = remember { mutableStateOf<Uri?>(null) } //imageuri is the image path(2)
+    val context = LocalContext.current
+
+
+    val pickImageLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia() //telling to pick the image(3)
+    ) {uri ->          //lambda function
+        if(uri != null){
+            imageUri.value = uri
+
+            val inputStream = uri.let { context.contentResolver.openInputStream(uri) }     //6
+            val byteArray = inputStream?.readBytes()   //7
+            image.value=byteArray  //8
+        }
+    }
     Column(modifier=Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
+
+        Button(              //4
+            onClick = {
+                pickImageLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }
+        ) {
+            Text(text = "Select Image")
+        }
+
+        if(imageUri != null){   //5
+            Image(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Selected Image",
+                modifier = Modifier.size(100.dp)
+            )
+        }else{
+            AsyncImage(
+                model=imageUri.value,
+                contentDescription = "Selected Image",
+                modifier = Modifier.size(100.dp)
+            )
+        }
+
+
+
+
 
         TextField(
             value = name.value,
@@ -57,7 +116,8 @@ fun AddContact(viewModel: MyViewModel?= hiltViewModel(), navController: NavContr
                 viewModel?.addContacts(
                     name = name.value,
                     phoneNumber = phoneNumber.value,
-                    email=email.value
+                    email=email.value,
+                    image=image.value
                 )
             }
         ) {
